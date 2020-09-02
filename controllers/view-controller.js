5287defa54d73726336892db0ede3af9032d552a
monolithgoon <svanteahrrenus@hotmail.com>
2020-09-02 01:59:28 +0100

    Added checkID middleware to view-controller.js && paramValue to args in renderParcelizedAgc

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -4,13 +4,41 @@ const catchAsync = require('../utils/catch-async.js') // TODO <
 
 
 
+// PARAM. MIDDLEWARE > VALIDATES THE ID FROM parcelized-agc-routes.js
+exports.checkID = async (req, res, next, paramValue) => {
+
+   try {
+
+      // console.log(`The AGC ID you are trying to validate is: ${req.params.id}`);
+      // console.log(`The AGC ID you are trying to validate is: ${paramValue}`);
+
+      const databaseQuery = await PARCELIZED_AGC_MODEL.findById(paramValue)
+
+      if (!databaseQuery) {
+         return res.status(404).json({
+            status: 'failed',
+            message: 'That was an invalid ID, you absolute cuck.'
+         })
+      }
+      
+      next();
+   }
+   
+   catch (err) {
+      console.log(err);
+   };
+}
+
+
+
+
 // this fn. looks in the "views" folder for the "overview" template && renders it to the browser
-exports.renderAllParcelizedAgcs = async (req, res, next) => {
+exports.renderAllParcelizedAgcs = async (req, res, next, paramValue) => {
 
    try {
 
       // 1. GET ALL THE PARCELIZED AGCS DATA FROM THE ATLAS DB
-      const parcelizedAgcs = await PARCELIZED_AGC_MODEL.find();
+      const parcelizedAgcs = await PARCELIZED_AGC_MODEL.findById(paramValue);
 
       // 2 BUILD TEMPLATE
 
@@ -35,6 +63,7 @@ exports.renderAllParcelizedAgcs = async (req, res, next) => {
 
 exports.renderParcelizedAgc = async (req, res) => {
    const parcelizedAgcs = await PARCELIZED_AGC_MODEL.find();
+   console.log(parcelizedAgcs)
    res.status(200).render('agc-render', {
       title: "Rendered AGC Farm Plots",
       parcelizedAgcsData: parcelizedAgcs
--- routes/view-routes.js
+++ routes/view-routes.js
@@ -5,7 +5,7 @@ const viewsController = require('../controllers/view-controller.js')
 
 
 ROUTER.get('/', viewsController.renderAllParcelizedAgcs)
-ROUTER.get('/parcelized-agc', viewsController.renderParcelizedAgc)
+ROUTER.get('/parcelized-agc/:id', viewsController.checkID, viewsController.renderParcelizedAgc)
 
 
 
