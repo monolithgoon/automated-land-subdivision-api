2c69698267e423da45c70938b73a73987b90c3f7
monolithgoon <svanteahrrenus@hotmail.com>
2020-09-07 16:14:10 +0100

    Corrected AGC data model

--- models/agc-model.js
+++ models/agc-model.js
@@ -44,15 +44,6 @@ const featureSchema = new mongoose.Schema({
       },
    geometry: geometrySchema,
    properties: {
-      agc_id: {
-         type: String,
-         required: [true, 'Each AGC must have an ID'],
-         // unique: [true, 'Each AGC must have a unique ID']
-         unique: true
-      },
-      location: String,
-      farmers: Array
-
       // chunk_index: {
       //    type: Number,
       //    required: [true, "Each farm parcel in the AGC must have a parcel index"],
@@ -100,11 +91,15 @@ const agcSchema = new mongoose.Schema({
       required: [true, `The featureCollection must have at least one feature or an array of features`],
       validate: [(entry) => Array.isArray(entry) && entry.length > 0, `The AGC featureCollection must have at least one feature or an array of features`],
    },
-   agc_id: {
-      type: String,
-      required: [true, 'Each AGC must have an ID'],
-      // unique: [true, 'Each AGC must have a unique ID']
-      unique: true
+   properties: {
+      agc_id: {
+         type: String,
+         required: [true, 'Each AGC must have an ID'],
+         // unique: [true, 'Each AGC must have a unique ID']
+         unique: true
+      },
+      location: String,
+      farmers: Array
    }
 });
 
