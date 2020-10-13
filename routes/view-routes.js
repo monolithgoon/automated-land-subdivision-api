0799531d1ebc5a07c38b7454d21f4c10fff5538d
monolithgoon <svanteahrrenus@hotmail.com>
2020-10-13 11:06:43 +0100

    Housekeeping

--- routes/view-routes.js
+++ routes/view-routes.js
@@ -5,8 +5,8 @@ const viewsController = require('../controllers/view-controller.js')
 
 
 ROUTER.get('/', viewsController.renderLandingPage);
-ROUTER.get('/api-guide', viewsController.renderAPIGuide);
 ROUTER.get('/agcs-overview', viewsController.renderOnlyAgcs);
+ROUTER.get('/api-guide', viewsController.renderAPIGuide);
 ROUTER.get('/parcelized-agc/:id', viewsController.checkDatabaseID, viewsController.renderParcelizedAgcByID);
 ROUTER.get('/parcelized-agc', viewsController.renderParcelizedAgc)
 
