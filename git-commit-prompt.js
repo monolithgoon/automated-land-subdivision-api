eb520529dd46fc229c68e6ac44b9c776b5369780
monolithgoon <svanteahrrenus@hotmail.com>
2023-04-02 08:14:21 +0100

    refactor (git-commit-prompt.js): add re-usable funcitons

--- git-commit-prompt.js
+++ git-commit-prompt.js
@@ -4,27 +4,27 @@ const readline = require("readline");
 const chalk = require("./utils/chalk-messages.js");
 
 /**
- * The execAsync() function is a utility function that wraps the `exec()` method from the `child_process` module in Node.js and returns a promise. 
+ * The execAsync() function is a utility function that wraps the `exec()` method from the `child_process` module in Node.js and returns a promise.
  * This allows you to execute shell commands asynchronously in a more structured and predictable way.
- * The function takes two arguments: `command` and `rl`. The `command` argument is a string that specifies the shell `command` to be executed. 
+ * The function takes two arguments: `command` and `rl`. The `command` argument is a string that specifies the shell `command` to be executed.
  * The `rl` argument is an optional readline interface that can be closed if the `command` execution fails.
  */
 function execAsync(command, rl) {
-  return new Promise((resolve, reject) => {
-    // Execute the command using the `exec()` method from the `child_process` module
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        rl.close();
-      } else if (stderr) {
+	return new Promise((resolve, reject) => {
+		// Execute the command using the `exec()` method from the `child_process` module
+		exec(command, (error, stdout, stderr) => {
+			if (error) {
+				reject(error);
+				rl.close();
+			} else if (stderr) {
 				reject(stderr);
-        rl.close();
-      } else {
-        // If there is no error, resolve the promise with the stdout
-        resolve(stdout);
-      }
-    });
-  });
+				rl.close();
+			} else {
+				// If there is no error, resolve the promise with the stdout
+				resolve(stdout);
+			}
+		});
+	});
 }
 
 function readlineQuestionAsync(question, rl) {
@@ -62,24 +62,18 @@ const COMMIT_TYPES = Object.freeze({
  * @returns {Promise<string>} - The validated user input as a string
  */
 async function askCommitPrompt(prompt, rl, promptFlag) {
-
 	// Prompt the user for input and await the response
 	let promptResponse = await readlineQuestionAsync(`${prompt}`, rl);
 
 	// Validate the user input based on the prompt flag
 	if (typeof promptResponse !== "string" || promptResponse.trim() === "") {
-
 		console.log(chalk.consoleYlow(`Response must be a non-empty string`));
 
 		// Recursively call the function until a valid input is received
 		promptResponse = askCommitPrompt(prompt, rl, promptFlag);
-
 	} else {
-
 		switch (promptFlag) {
-
 			case "TYPE":
-
 				// Check if the input is at least 2 characters long
 				if (promptResponse.length < 2) {
 					console.log(chalk.consoleYlow("Commit type must be at least 2 characters long"));
@@ -87,15 +81,14 @@ async function askCommitPrompt(prompt, rl, promptFlag) {
 				}
 
 				// Check if the input is a valid commit type
-					if (!Object.values(COMMIT_TYPES).includes(promptResponse.toLowerCase())) {
-						console.log(chalk.consoleYlow(`Invalid input. Please enter a correct type:`));
-						console.log(COMMIT_TYPES);
-						promptResponse = await askCommitPrompt(prompt, rl, promptFlag);
-					}
-					break;
+				if (!Object.values(COMMIT_TYPES).includes(promptResponse.toLowerCase())) {
+					console.log(chalk.consoleYlow(`Invalid input. Please enter a correct type:`));
+					console.log(COMMIT_TYPES);
+					promptResponse = await askCommitPrompt(prompt, rl, promptFlag);
+				}
+				break;
 
 			case "DOMAIN":
-
 				// Check if the input is at least 3 characters long
 				if (promptResponse.length < 3) {
 					console.log(chalk.consoleYlow("Commit domain must be at least 3 characters long"));
@@ -104,7 +97,6 @@ async function askCommitPrompt(prompt, rl, promptFlag) {
 				break;
 
 			case "MESSAGE":
-
 				// Check if the input is at least 10 characters long
 				if (promptResponse.length < 10) {
 					console.log(chalk.consoleYlow("Commit message must be at least 10 characters long"));
@@ -113,35 +105,22 @@ async function askCommitPrompt(prompt, rl, promptFlag) {
 				break;
 
 			case "CONFIRM":
-
 				// Check if the input is a valid confirmation response
-				if (
-					!["yes", "y", "no", "n", "quit", "end", "close"].includes(
-						promptResponse.toLowerCase()
-					)
-				) {
-					console.log(
-						chalk.consoleYlow("Invalid input. Please enter 'Y', 'N', 'END' or 'QUIT'")
-					);
+				if (!["yes", "y", "no", "n", "quit", "end", "close"].includes(promptResponse.toLowerCase())) {
+					console.log(chalk.consoleYlow("Invalid input. Please enter 'Y', 'N', 'END' or 'QUIT'"));
 					promptResponse = await askCommitPrompt(prompt, rl, promptFlag);
 				}
 				break;
 
 			case "AMEND":
-
 				// Check if the input is a valid amend type
 				if (!["TYPE", "DOMAIN", "MESSAGE", "NONE"].includes(promptResponse.toUpperCase())) {
-					console.log(
-						chalk.consoleYlow(
-							"Invalid input. Please enter 'TYPE', 'DOMAIN', 'MESSAGE' or 'NONE'"
-						)
-					);
+					console.log(chalk.consoleYlow("Invalid input. Please enter 'TYPE', 'DOMAIN', 'MESSAGE' or 'NONE'"));
 					promptResponse = await askCommitPrompt(prompt, rl, promptFlag);
 				}
 				break;
 
 			case "ORIGIN":
-
 				if (!["yes", "y", "no", "n"].includes(promptResponse.toLowerCase())) {
 					console.log(chalk.consoleYlow("Invalid input. Please enter 'Y' or 'N'"));
 					promptResponse = await askCommitPrompt(prompt, rl, promptFlag);
@@ -164,6 +143,39 @@ async function writeLocalCommit(commitMsg, readLineInterface) {
 	} catch (error) {
 		console.error(chalk.warningBright(`local commit error: ${error}`));
 	}
+}
+
+async function writeOriginCommit(readLineInterface) {
+	console.log(chalk.consoleGy("Committing to origin .."));
+	try {
+		const pushOriginResponse = await execAsync(`git push origin master`, readLineInterface);
+		console.log(`pushOriginResponse:`);
+		console.log(chalk.consoleG(pushOriginResponse));
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
+
+async function forceOriginCommit(readLineInterface) {
+	
+	const askForceCommitOrigin = await askCommitPrompt("Force push commit to remote origin? ( Y / N )", readLineInterface, "ORIGIN");
+
+	if (["yes", "y"].includes(askForceCommitOrigin.toLowerCase())) {
+		console.log(chalk.consoleGy("Force pushing to origin .."));
+
+		try {
+			pushOriginResponse = await execAsync(`git push origin master --force`, readLineInterface);
+			console.log({ pushOriginResponse });
+		} catch (error) {
+			console.error(chalk.fail(`origin commit error: ${error}`));
+			process.exit(0);
+		}
+	}
+
+	console.log("yeo are here");
+	rl.close();
+	process.exit(0);
 };
 
 /**
@@ -172,7 +184,6 @@ async function writeLocalCommit(commitMsg, readLineInterface) {
  * @function executeCommitPrompts
  */
 async function executeCommitPrompts() {
-
 	// Create a readline interface to prompt the user for input
 	const rl = readline.createInterface({
 		input: process.stdin,
@@ -189,16 +200,15 @@ async function executeCommitPrompts() {
 		completeCommitMsg,
 		commitConfirm,
 		commitAmendChoice,
-		commitResponse,
-		okCommitOrigin,
-		okForceCommitOrigin,
+		// commitResponse,
+		askOriginCommit,
+		originCommitOk,
+		askForceCommitOrigin,
 		pushOriginResponse;
 
 	try {
-
 		// Prompt the user for commit information until they confirm their message
 		while (true) {
-
 			// Check if the user has requested to change a specific part of the commit message
 			switch (commitAmendChoice?.toUpperCase()) {
 				case "TYPE":
@@ -230,11 +240,7 @@ async function executeCommitPrompts() {
 			console.log({ completeCommitMsg });
 
 			// Confirm the commit message with the user
-			commitConfirm = await askCommitPrompt(
-				"Confirm commit message is OK? ( Y / N / QUIT):",
-				rl,
-				"CONFIRM"
-			);
+			commitConfirm = await askCommitPrompt("Confirm commit message is OK? ( Y / N / QUIT):", rl, "CONFIRM");
 
 			if (["yes", "y"].includes(commitConfirm.toLowerCase())) {
 				// Break out of while loop
@@ -257,7 +263,7 @@ async function executeCommitPrompts() {
 
 		// // REMOVE
 		// console.log(chalk.consoleGy("Writing local commit .."));
-		
+
 		// // REMOVE
 		// // Add and commit the changes using the complete commit message
 		// try {
@@ -272,23 +278,20 @@ async function executeCommitPrompts() {
 		await writeLocalCommit(completeCommitMsg, rl);
 
 		// Prompt user to commit to origin / master
-		okCommitOrigin = await askCommitPrompt(
-			"Push commit to remote origin? ( Y / N )",
-			rl,
-			"ORIGIN"
-		);
+		askOriginCommit = await askCommitPrompt("Push commit to remote origin? ( Y / N )", rl, "ORIGIN");
 
-		async function writeOriginCommit(commitMsg) {
-			
-		}
+		originCommitOk = writeOriginCommit(rl);
+
+		if (!originCommitOk) forceOriginCommit(rl);
+
+		process.exit(0);
 
 		// User chooses to commit to remote origin
-		if (["yes", "y"].includes(okCommitOrigin.toLowerCase())) {
-
+		if (["yes", "y"].includes(askOriginCommit.toLowerCase())) {
 			console.log(chalk.consoleGy("Committing to origin .."));
 
 			pushOriginResponse = await execAsync(`git push origin master`, rl);
-			
+
 			console.log(`pushOriginResponse:`);
 			console.log(chalk.consoleG(pushOriginResponse));
 			// try {
@@ -301,8 +304,8 @@ async function executeCommitPrompts() {
 			// 	// Give option to user to force the commit to origin
 			// 	console.log(error)
 			// 	// if (error.message.toLowerCase().includes("command failed")) {
-			// 	// 	const okForceCommitOrigin = await askCommitPrompt("Force push commit to remote origin? ( Y / N )", rl, "ORIGIN");
-			// 	// 	if (["yes", "y"].includes(okForceCommitOrigin.toLowerCase())) {
+			// 	// 	const askForceCommitOrigin = await askCommitPrompt("Force push commit to remote origin? ( Y / N )", rl, "ORIGIN");
+			// 	// 	if (["yes", "y"].includes(askForceCommitOrigin.toLowerCase())) {
 			// 	// 		console.log(chalk.consoleGy("Committing to origin .."));
 			// 	// 		try {
 			// 	// 			pushOriginResponse = await execAsync(`git push origin master`, rl);
@@ -310,7 +313,7 @@ async function executeCommitPrompts() {
 			// 	// 			console.log(chalk.consoleG(pushOriginResponse));
 			// 	// 		} catch (error) {
 			// 	// 			console.error(chalk.fail(`origin commit error: ${error}`));
-			// 	// 		}		
+			// 	// 		}
 			// 	// 	}
 			// 	// }
 			// }	finally {
@@ -318,34 +321,31 @@ async function executeCommitPrompts() {
 			// 	process.exit();
 			// }
 		} else {
-
 		}
-
 	} catch (error) {
-
 		console.error(chalk.fail(`executeCommitPrompts error`));
 		console.error(chalk.consoleYlow(error.message));
 
-		if (error.message.toLowerCase().includes("command failed")) {
+		// if (error.message.toLowerCase().includes("command failed")) {
 
-			okForceCommitOrigin = await askCommitPrompt("Force push commit to remote origin? ( Y / N )", rl, "ORIGIN");
+		// 	askForceCommitOrigin = await askCommitPrompt("Force push commit to remote origin? ( Y / N )", rl, "ORIGIN");
 
-			if (["yes", "y"].includes(okForceCommitOrigin.toLowerCase())) {
+		// 	if (["yes", "y"].includes(askForceCommitOrigin.toLowerCase())) {
 
-				console.log(chalk.consoleGy("Committing to origin .."));
+		// 		console.log(chalk.consoleGy("Committing to origin .."));
 
-				try {
-					pushOriginResponse = await execAsync(`git push origin master --force`, rl);
-					console.log({ pushOriginResponse });
-				} catch (error) {
-					console.error(chalk.fail(`origin commit error: ${error}`));
-				}
-			} 
-			
-			console.log("yeo are here")
-			rl.close();
-			process.exit();
-		}
+		// 		try {
+		// 			pushOriginResponse = await execAsync(`git push origin master --force`, rl);
+		// 			console.log({ pushOriginResponse });
+		// 		} catch (error) {
+		// 			console.error(chalk.fail(`origin commit error: ${error}`));
+		// 		}
+		// 	}
+
+		// 	console.log("yeo are here")
+		// 	rl.close();
+		// 	process.exit(0);
+		// }
 	} finally {
 		// Close the readline interface and exit the process
 		rl.close();
