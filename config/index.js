365655fff27819733745c60b5fd8b4467a96d484
monolithgoon <svanteahrrenus@hotmail.com>
2023-10-22 13:43:31 +0100

    refactor (config/index.js): changed 'PORT' to 'AUTO_LAND_SUBDIVISION_PORT'

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const dotenv = require(`dotenv`);
 dotenv.config({ path: path.resolve(__dirname, "../default.env") });
 
 module.exports = Object.freeze({
-	port: parseInt(process.env.PORT, 10) || 9443,
+	port: parseInt(process.env.AUTO_LAND_SUBDIVISION_PORT, 10) || 9443,
 	cloudinaryUrl: process.env.CLOUDINARY_URL,
 	turfPolygonBufferUnits: process.env.TURF_POLYGON_BUFFER_UNITS,
   minimumBufferArea: process.env.TURF_POLYGON_MINIMUM_BUFFER_AREA,
